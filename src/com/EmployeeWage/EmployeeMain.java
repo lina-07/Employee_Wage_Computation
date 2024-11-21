cc181b19142c3836f03558a319727747f8e8dcab
lina <linabirari74@gmail.com>
2024-11-21 14:34:04 +0530

    Welcome to Employee Wage Computation

---
+++ src/com/EmployeeWage/EmployeeMain.java
@@ -0,0 +1,8 @@
+package com.EmployeeWage;
+
+public class EmployeeMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+
+    }
+}
