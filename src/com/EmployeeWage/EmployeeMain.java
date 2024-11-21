87af16e0892c900de9c8f7c7b64cffcfcc5cbe3c
lina <linabirari74@gmail.com>
2024-11-21 17:47:27 +0530

    UC-5 Calculating wages for a month

--- src/com/EmployeeWage/EmployeeMain.java
+++ src/com/EmployeeWage/EmployeeMain.java
@@ -15,6 +15,7 @@ public class EmployeeMain {
                     int wagePerHour = 20;
                     int fullDayHours = 8;
                     int partTimeHours = 4;
+                    int workingDaysPerMonth = 20;
 
                     int employeeType;
                     if (random.nextBoolean()) {
@@ -25,14 +26,18 @@ public class EmployeeMain {
               switch (employeeType) {
                case 1: // Full-Time
                  int dailyWage = wagePerHour * fullDayHours;
+                 int monthlyWageFullTime = dailyWage * workingDaysPerMonth;
                  System.out.println("Employee is working Full-Time");
                  System.out.println("Daily Employee Wage: " + dailyWage);
+                 System.out.println("Monthly Employee Wage: " + monthlyWageFullTime);
                  break;
 
                case 2:
                   int partTimeWage = wagePerHour * partTimeHours;
+                  int monthlyWagePartTime = partTimeWage * workingDaysPerMonth;
                   System.out.println("Employee is working Part-Time");
                   System.out.println("Daily Part-Time Employee Wage: " + partTimeWage);
+                  System.out.println("Monthly Part-Time Employee Wage: " + monthlyWagePartTime);
                    break;
 
                default:
