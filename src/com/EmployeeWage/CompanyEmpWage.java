d92a5da4a7057acf36b78f44dc745133ddbe77e4
lina <linabirari74@gmail.com>
2024-11-25 17:13:18 +0530

    UC-10 Ability to manage employee wage of multiple companies

--- src/com/EmployeeWage/CompanyEmpWage.java
+++ src/com/EmployeeWage/CompanyEmpWage.java
@@ -1,4 +1,47 @@
 package com.EmployeeWage;
 
-public class CompanyEmpWage {
-}
+class CompanyEmpWage {
+        private String companyName;
+        private int wagePerHour;
+        private int workingDaysPerMonth;
+        private int maxWorkingHours;
+        private int totalWages;
+
+        public CompanyEmpWage(String companyName, int wagePerHour, int workingDaysPerMonth, int maxWorkingHours) {
+            this.companyName = companyName;
+            this.wagePerHour = wagePerHour;
+            this.workingDaysPerMonth = workingDaysPerMonth;
+            this.maxWorkingHours = maxWorkingHours;
+        }
+
+        // Getters and setters
+        public String getCompanyName() {
+            return companyName;
+        }
+
+        public int getWagePerHour() {
+            return wagePerHour;
+        }
+
+        public int getWorkingDaysPerMonth() {
+            return workingDaysPerMonth;
+        }
+
+        public int getMaxWorkingHours() {
+            return maxWorkingHours;
+        }
+
+        public int getTotalWages() {
+            return totalWages;
+        }
+
+        public void setTotalWages(int totalWages) {
+            this.totalWages = totalWages;
+        }
+
+        @Override
+        public String toString() {
+            return "Company: " + companyName + ", Total Wages: " + totalWages;
+        }
+    }
+
