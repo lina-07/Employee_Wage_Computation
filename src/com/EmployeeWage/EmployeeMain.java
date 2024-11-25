563cd8785f0cb86b664030f14dd014a6bbba2d54
lina <linabirari74@gmail.com>
2024-11-25 16:08:21 +0530

    UC-9 Abiliity to save total wage for each company

--- src/com/EmployeeWage/EmployeeMain.java
+++ src/com/EmployeeWage/EmployeeMain.java
@@ -5,67 +5,79 @@ import java.util.Random;
 public class EmployeeMain {
                 public static void main(String[] args) {
                     System.out.println("Welcome to Employee Wage Computation");
-                    Employee employee = new Employee();
+                    EmpWageBuilder companyA = new EmpWageBuilder("Company A", 20, 20, 100);
+                    EmpWageBuilder companyB = new EmpWageBuilder("Company B", 25, 22, 120);
+                    EmpWageBuilder companyC = new EmpWageBuilder("Company C", 18, 25, 90);
 
-                    System.out.println("\nCompany A:");
-                    employee.computeWage("Company A", 20, 20, 100);
+                    companyA.computeWage();
+                    companyB.computeWage();
+                    companyC.computeWage();
 
-                    System.out.println("\nCompany B:");
-                    employee.computeWage("Company B", 25, 22, 120);
-
-                    System.out.println("\nCompany C:");
-                    employee.computeWage("Company C", 18, 25, 90);
+                    System.out.println("\nSummary of Total Wages for Each Company:");
+                    System.out.println(companyA);
+                    System.out.println(companyB);
+                    System.out.println(companyC);
                 }
 }
 
-                    class Employee {
+                    class EmpWageBuilder{
 
-                    private int fullDayHours = 8;
-                    private int partTimeHours = 4;
+                        private String companyName;
+                        private int wagePerHour;
+                        private int workingDaysPerMonth;
+                        private int maxWorkingHours;
+                        private int totalWages;
 
-                    public void computeWage(String companyName, int wagePerHour, int workingDaysPerMonth, int maxWorkingHours){
+                        private int fullDayHours = 8;
+                        private int partTimeHours = 4;
+
+                        public EmpWageBuilder(String companyName, int wagePerHour, int workingDaysPerMonth, int maxWorkingHours) {
+                            this.companyName = companyName;
+                            this.wagePerHour = wagePerHour;
+                            this.workingDaysPerMonth = workingDaysPerMonth;
+                            this.maxWorkingHours = maxWorkingHours;
+                        }
+                    public void computeWage() {
                         int totalWorkingHours = 0;
                         int totalWorkingDays = 0;
-                        int totalWages = 0;
 
-                    Random random = new Random();
-                    boolean isPresent = random.nextBoolean();
 
-                    if (isPresent) {
-                        System.out.println("Employee is Present");
+                        Random random = new Random();
+                        System.out.println("\nComputing Wages for " + companyName);
 
-                        while (totalWorkingHours < 100 && totalWorkingDays < workingDaysPerMonth)
-                        {
-                        int employeeType;
 
-                        if (random.nextBoolean()) {
-                            employeeType = 1; // Full-Time
-                        } else {
-                            employeeType = 2; // Part-Time
-                        }
-                        int hoursWorked;
-                            if (employeeType == 1) {
-                                hoursWorked = fullDayHours;
-                            } else {
-                                hoursWorked = partTimeHours;
+
+                            while (totalWorkingHours < maxWorkingHours && totalWorkingDays < workingDaysPerMonth) {
+                                int employeeType;
+
+                                if (random.nextBoolean()) {
+                                    employeeType = 1; // Full-Time
+                                } else {
+                                    employeeType = 2; // Part-Time
+                                }
+                                int hoursWorked;
+                                if (employeeType == 1) {
+                                    hoursWorked = fullDayHours;
+                                } else {
+                                    hoursWorked = partTimeHours;
+                                }
+
+                                if (totalWorkingHours + hoursWorked > maxWorkingHours) {
+                                    hoursWorked = maxWorkingHours - totalWorkingHours;
+                                }
+
+                                totalWorkingHours += hoursWorked;
+                                totalWorkingDays++;
+
                             }
+                            totalWages += totalWorkingHours * wagePerHour;
 
-                            if (totalWorkingHours + hoursWorked > 100) {
-                                hoursWorked = 100 - totalWorkingHours;
-                            }
-
-                            totalWorkingHours += hoursWorked;
-                            totalWages += hoursWorked * wagePerHour;
-                            totalWorkingDays++;
+                            System.out.println("Total Days Worked: " + totalWorkingDays);
+                            System.out.println("Total Hours Worked: " + totalWorkingHours);
+                            System.out.println("Total Wages: " + totalWages);
 
                         }
-
-                        System.out.println("Total Days Worked: " + totalWorkingDays);
-                        System.out.println("Total Hours Worked: " + totalWorkingHours);
-                        System.out.println("Total Wages: " + totalWages);
-
-        } else {
-            System.out.println("Employee is Absent");
-        }
-    }
-}
+                        public String toString () {
+                            return "Company: " + companyName + ", Total Wages: " + totalWages;
+                        }
+                    }
